41e55bcf120aa1ce48b9f12b8eae87c036d6319b
Ferlo94 <laferlart@gmail.com>
2022-05-09 19:14:38 +0200

    controllo utente implementato

--- snack-2/script.js
+++ snack-2/script.js
@@ -13,18 +13,24 @@ function pariDispari(numero){
 
 // MAIN
 
-// 1 - l'utente inserisce pari o dispari
-// 2 - l'utente inserisce pari o dispari
-// 3 - generiamo un numero random per la cpu
-// 4 - sommiamo i due numeri
-// 5 - verifichiamo se la somma e' pari o dispari
-// 6 - Dichiariamo chi ha vinto
-
+let sceltaUtente;
 
 // pari o dispari
-const sceltaUtente = prompt("Scegli tra pari e dispari");
+do {
+    
+    sceltaUtente = prompt("Scegli tra pari e dispari");
+} while (sceltaUtente !== "pari" && sceltaUtente !== "dispari");
+
 // numero da 1 a 5
-const sceltaNumero = Number(prompt("Scegli un numero da 1 a 5"));
+
+let sceltaNumero
+
+do {
+    sceltaNumero = Number(prompt("Scegli un numero da 1 a 5"));
+    
+} while(isNaN(sceltaNumero) || sceltaNumero < 1 || sceltaNumero > 5);
+
+
 // Numero random
 const cpuNumber = randomGenerator(1,5);
 // somma dei due numeri
